ad56ef36c2fdf26da901775c0399e0567c37f482
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2023-03-26 21:48:56 +0300

    Implement animations change

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,6 +7,8 @@ const FRICTION = 1000.0
 const JUMP_VELOCITY = -40000.0
 
 @onready var animation_player = $AnimationPlayer
+@onready var animation_tree = $AnimationTree
+@onready var animation_state = animation_tree.get("parameters/playback") as AnimationNodeStateMachinePlayback
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -15,13 +17,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animation_player.play("run_right")
-		else:
-			animation_player.play("run_left")
+		animation_tree.set("parameters/idle/blend_position", input_vector)
+		animation_tree.set("parameters/run/blend_position", input_vector)
+		animation_state.travel("run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		animation_player.play("idle_right")
+		animation_state.travel("idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move_and_slide()
