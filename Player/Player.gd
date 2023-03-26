b394c6934bab9f23a6702bd01b4967407b1965c9
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2023-03-26 21:16:16 +0300

    Implement subscenes animations

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,6 +6,7 @@ const MAX_SPEED = 120.0
 const FRICTION = 1000.0
 const JUMP_VELOCITY = -40000.0
 
+@onready var animation_player = $AnimationPlayer
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -14,8 +15,13 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animation_player.play("run_right")
+		else:
+			animation_player.play("run_left")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animation_player.play("idle_right")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move_and_slide()
