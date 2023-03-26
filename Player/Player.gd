9398cfe36552d511ba1fa35c5806fe0f93923e8b
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2023-03-26 18:59:44 +0300

    Basic character movement and collisions

---
+++ Player/Player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+const ACCELERATION = 700.0
+const MAX_SPEED = 120.0
+const FRICTION = 1000.0
+const JUMP_VELOCITY = -40000.0
+
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	move_and_slide()
