04200701236318c6d48c355d016af6892b9a659d
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2023-03-29 13:39:40 +0300

    Add attack animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,19 +6,36 @@ const MAX_SPEED = 120.0
 const FRICTION = 1000.0
 const JUMP_VELOCITY = -40000.0
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+
 @onready var animation_player = $AnimationPlayer
 @onready var animation_tree = $AnimationTree
 @onready var animation_state = animation_tree.get("parameters/playback") as AnimationNodeStateMachinePlayback
 
+func _ready():
+	animation_tree.active = true
+
 func _physics_process(delta):
+	match state:
+		MOVE: move_state(delta)
+		ATTACK: attack_state(delta)
+		ROLL: roll_state(delta)
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		animation_tree.set("parameters/idle/blend_position", input_vector)
-		animation_tree.set("parameters/run/blend_position", input_vector)
+		update_direction(input_vector)
+		
 		animation_state.travel("run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -26,3 +43,21 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func attack_state(delta):
+	# velocity = Vector2.ZERO # this should fix slide on attack, but I do not have this bug
+	animation_state.travel("attack")
+	
+func attack_animation_finished():
+	state = MOVE
+	
+func roll_state(delta):
+	pass
+	
+func update_direction(input_vector):
+	animation_tree.set("parameters/idle/blend_position", input_vector)
+	animation_tree.set("parameters/run/blend_position", input_vector)
+	animation_tree.set("parameters/attack/blend_position", input_vector)
